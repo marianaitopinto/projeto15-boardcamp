97d435a8b4604c1cab8632b3d3606c8b6d3374b2
Mariana Ito <marianaitopinto@gmail.com>
2022-05-29 19:04:30 -0300

    creating rules for creating games

---
+++ src/middlewares/gamesMiddleware.js
@@ -0,0 +1,43 @@
+import db from '../db.js';
+
+import gamesSchema from '../schemas/gamesSchema.js';
+
+export async function validateGame(req, res, next) {
+    const game = req.body;
+
+    const gameValidation = gamesSchema.validate(game, {abortEarly: false});
+
+    if (gameValidation.error) {
+        const validationErrors = gameValidation.error.details.map(detail => detail.message);
+        console.log(`Validation errors: `, validationErrors);
+        return res.status(422).send(validationErrors);
+    }
+
+    if ((parseInt(game.stockTotal) <= 0) || (parseInt(game.pricePerDay) <= 0) || (game.name === "")) {
+        return res.sendStatus(400);
+    }
+
+    try {
+        const checkCategoryExists = await db.query(`
+        SELECT * FROM categories 
+        WHERE id=$1
+        `, [game.categoryId]);
+
+        if (checkCategoryExists.rowCount === 0) {
+            return res.sendStatus(400);
+        }
+
+        const checkGameExists = await db.query(`
+        SELECT * FROM games 
+        WHERE name=$1
+        `, [game.name]);
+
+        if (checkGameExists.rowCount > 0) {
+            return res.sendStatus(409);
+        }
+
+        next();
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
--- src/routes/gamesRouter.js
+++ src/routes/gamesRouter.js
@@ -2,9 +2,11 @@ import { Router } from 'express';
 
 import {listGames, createGames} from '../controllers/gamesController.js';
 
+import { validateGame } from '../middlewares/gamesMiddleware.js';
+
 const gamesRouter = Router();
 
 gamesRouter.get('/games', listGames);
-gamesRouter.post('/games', createGames);
+gamesRouter.post('/games', validateGame, createGames);
 
 export default gamesRouter;
