9dae0354c30a84eb08b93a419327beb9cf6cb92c
Mariana Ito <marianaitopinto@gmail.com>
2022-05-30 13:33:13 -0300

    fixing bug get customer

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -2,13 +2,13 @@ import db from "./../db.js";
 
 export async function getCustomers(req, res) {
     const { cpf } = req.query;
-    
+
     try {
         if (!cpf) {
-        const customers = await db.query(`
+            const customers = await db.query(`
             SELECT * FROM customers
         `)
-        res.send(customers.rows);
+            res.send(customers.rows);
         } else {
             const customers = await db.query(`
                 SELECT * FROM customers
@@ -22,7 +22,7 @@ export async function getCustomers(req, res) {
 }
 
 export async function createCustomer(req, res) {
-    const {name, phone, cpf, birthday} = req.body;
+    const { name, phone, cpf, birthday } = req.body;
 
     try {
         await db.query(`
@@ -40,25 +40,25 @@ export async function createCustomer(req, res) {
 export async function getCustomerId(req, res) {
     const { id } = req.params;
 
-    try{
+    try {
         const customer = await db.query(`
             SELECT * FROM customers WHERE id=$1
         `, [id]);
 
-        if(customer.rowCount === 0){
+        if (customer.rowCount === 0) {
             return res.sendStatus(404);
         }
-        
+
         res.status(200).send(customer.rows[0]);
     } catch (error) {
         console.log(error)
         res.status(500).send(error);
     }
-    
+
 }
 
 export async function updateCustomer(req, res) {
-    const {name, phone, cpf, birthday} = req.body;
+    const { name, phone, cpf, birthday } = req.body;
     const { id } = req.params;
 
     try {
