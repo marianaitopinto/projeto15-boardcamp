0e6ad0f0c70ce2a0302b338d07bdf8b2b060b5ee
Mariana Ito <marianaitopinto@gmail.com>
2022-05-30 13:32:22 -0300

    fixing bugs get customer

--- src/controllers/customersController.js
+++ src/controllers/customersController.js
@@ -1,12 +1,21 @@
 import db from "./../db.js";
 
 export async function getCustomers(req, res) {
+    const { cpf } = req.query;
+    
     try {
+        if (!cpf) {
         const customers = await db.query(`
             SELECT * FROM customers
         `)
-
         res.send(customers.rows);
+        } else {
+            const customers = await db.query(`
+                SELECT * FROM customers
+                WHERE cpf LIKE $1`, [`${cpf}%`]
+            );
+            res.send(customers.rows);
+        }
     } catch (error) {
         res.status(500).send(error);
     }
